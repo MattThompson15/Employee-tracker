0f2e35388f297d249f195b3b581ad90aab689961
Matt Thompson <matthewthompson118@gmail.com>
2023-12-26 15:20:22 -0400

    seeds.sql, modifies .json

---
+++ seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO department (id, name) VALUES
+(1, 'Sales'),
+(2, 'Engineering'),
+(3, 'Finance'):
+
+INSERT INTO role (id, title, salary, department_id) VALUES
+
+(1, 'Sales Manager', 60000, 1),
+(2, 'Software Engineer', 80000, 2),
+(3, 'Accountant', 50000, 3);
