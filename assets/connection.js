f51d01dcfea09868e63ae4b062a392a7b96f7613
Matt Thompson <matthewthompson118@gmail.com>
2023-12-27 20:56:46 -0400

    got app functioning

--- assets/connection.js
+++ assets/connection.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2')
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'Fit4li$e',
+    password: 'Mgt95543',
     database: 'employee_db',
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database: ' + err.stack);
+        return;
+    }
+    console.log('Connected to the database with ID ' + connection.threadId);
 });
 
 module.exports = connection;
